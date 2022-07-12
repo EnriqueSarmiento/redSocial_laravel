0dc8dd3dd21d061fb66cdd077cedc8c86fdf9668
Enrique Sarmiento <jeisonsarmiento@correo.unimet.edu.ve>
2022-07-12 16:00:18 -0400

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>DevStargram - @yield('titulo')</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="antialiased bg-gray-100">
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">DevStagram</h1>
+                <nav>
+                    <a class="font-bold uppercase text-gray-500 text-sm" href="">login</a>
+                    <a class="font-bold uppercase text-gray-500 text-sm" href="">crear cuenta</a>
+                </nav>
+            </div>
+        </header>
+
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">@yield('titulo')</h2>
+            @yield('contenido')
+        </main>
+
+        <footer class="text-center uppercase p-5 text-gray-500 font-bold">
+            DevStagram - todos los derechos reservados {{ now()->year }}.
+        </footer>
+    </body>
+
+</html>
---
+++ resources/views/principal.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('titulo')
+    pagina principal
+@endsection
+@section('contenido')
+    <div>
+        <p>este es el contendio de la pagina</p>
+    </div>
+@endsection
